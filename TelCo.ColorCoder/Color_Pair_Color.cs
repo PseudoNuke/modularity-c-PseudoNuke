1974f8079f328d817264d650e91f6a2386d2319a
PseudoNuke <thilakashwin10@gmail.com>
2020-08-25 21:10:44 +0530

    Create Color_Pair_Color.cs

---
+++ TelCo.ColorCoder/Color_Pair_Color.cs
@@ -0,0 +1,46 @@
+using System;
+using static TelCo.ColorCoder.ColorSpace;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    class Color_Pair_Color
+    {
+        private ColorSpace space;
+        public Color_Pair_Color()
+        {
+            space = new ColorSpace();
+        }
+
+        public int GetPairNumberFromColor(ColorPair pair)
+        {
+            int majorIndex = -1;
+            for (int i = 0; i < space.getColorMapMajor().Length; i++)
+            {
+                if ((Color)space.getColorMapMajor().GetValue(i) == pair.majorColor)
+                {
+                    majorIndex = i;
+                    break;
+                }
+            }
+
+            int minorIndex = -1;
+            for (int i = 0; i < space.getColorMapMinor().Length; i++)
+            {
+                if ((Color)space.getColorMapMinor().GetValue(i) == pair.minorColor)
+                {
+                    minorIndex = i;
+                    break;
+                }
+            }
+            if (majorIndex == -1 || minorIndex == -1)
+            {
+                throw new ArgumentException(
+                    string.Format("Unknown Colors: {0}", pair.ToString()));
+            }
+
+            
+            return (majorIndex * space.getColorMapMinor().Length) + (minorIndex + 1);
+        }
+    }
+}
