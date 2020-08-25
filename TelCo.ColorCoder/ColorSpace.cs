1b6809933fa79c052efb75c41fa3b1f9adfc6bb3
PseudoNuke <thilakashwin10@gmail.com>
2020-08-25 21:03:52 +0530

    Create ColorSpace.cs

---
+++ TelCo.ColorCoder/ColorSpace.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace TelCo.ColorCoder
+{
+    class ColourSpace
+    {
+        protected static Color[] colorMapMajor;
+
+        protected  static Color[] colorMapMinor;
+
+        internal class ColorPair
+        {
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
+
+        static ColorSpace()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        public Color[] getColorMapMajor()
+        {
+            return colorMapMajor;
+        }
+
+        public Color[] getColorMapMinor()
+        {
+            return colorMapMinor;
+        }
+
+    }
+}
